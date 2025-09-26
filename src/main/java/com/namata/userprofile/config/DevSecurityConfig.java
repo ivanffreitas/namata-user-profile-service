3685b75c6ff5d4b4d0fefb96962ae02461a9af4c
ivanffreitas <ivanff.ti@gmail.com>
2025-09-25 21:53:59 -0300

    Remove

--- src/main/java/com/namata/userprofile/config/DevSecurityConfig.java
+++ src/main/java/com/namata/userprofile/config/DevSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Profile("dev")
+@Profile({"dev", "docker"})
 public class DevSecurityConfig {
 
     @Bean
